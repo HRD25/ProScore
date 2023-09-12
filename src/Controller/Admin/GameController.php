92d7ada0d9d4488f1a90903761a5455f686890b0
rysiu <rysiu@jellydev.solutions>
2023-09-12 22:20:35 +0200

    add GameController, GameEntity, GameRepository

--- src/Controller/Admin/GameController.php
+++ src/Controller/Admin/GameController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Admin\Game;
+namespace App\Controller\Admin;
 
 use App\Service\PageParamsServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
